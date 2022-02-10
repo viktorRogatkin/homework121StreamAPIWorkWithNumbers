5e8e6ef1cdd5e4e250d750aad1e188b5ddb0dd49
Viktor Rogatkin <rogatkin.viktor@mail.ru>
2022-02-10 13:19:11 +0300

    homework12WorkWithNumbers

---
+++ Main.java
@@ -0,0 +1,23 @@
+package homework12WorkWithNumbers;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] input = {1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4};
+        List<Integer> result = new ArrayList<>();
+        for (int x : input) {
+            if (x > 0) {
+                if (x % 2 == 0) {
+                    result.add(x);
+                } else {
+                    continue;
+                }
+            } else {
+                continue;
+            }
+            Collections.sort(result);
+        }
+        System.out.println(result);
+    }
+}
